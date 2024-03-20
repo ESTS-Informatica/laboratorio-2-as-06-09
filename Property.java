b7d0716902e034f04887405bdbd847c321143ada
Donizete Francisco <100934199+Donizetefrancisco@users.noreply.github.com>
2024-03-20 11:05:49 +0000

    nivel1

--- Property.java
+++ Property.java
@@ -9,7 +9,7 @@
 public class Property {
 
     private static int numberOfProperties = 0;
-    private final String id;
+    private final int id;
     private String description;
     private double price;
 
@@ -20,21 +20,24 @@ public class Property {
      * @param price The property price.
      */
     public Property(String description, double price) {
-
+        setPrice(price);
+        setDescription(description);
+        numberOfProperties++;
+        id = numberOfProperties;
     }
 
     /**
      * Id selector.
      */
     public int getId() {
-        
+       return id; 
     }
 
     /**
      * Description selector.
      */
     public String getDescription() {
-
+       return description;
     }
 
     /**
@@ -43,14 +46,16 @@ public class Property {
      * @param description The new description. Must not be null.
      */
     public void setDescription(String description) {
-
+        if(description != null){
+           this.description = description;
+        }
     }
 
     /**
      * Price selector.
      */
     public double getPrice() {
-        
+        return price;
     }
     
     /**
@@ -59,10 +64,13 @@ public class Property {
      * @param price The new price. Must not be negative.
      */
     public void setPrice(double price) {
-
+      if(price >= 0.0){
+         this.price = price; 
+      }
     }
 
     @Override
     public String toString() {
+        return "";
     }
 }
